7aac49f6255e5baf38862a74be05862e8bb53b7f
Bob Gan <Bob Gan@LAPTOP-CF774SSM>
2026-03-09 15:50:56 +0800

    5th Commit

--- src/dsa/asg/main.java
+++ src/dsa/asg/main.java
@@ -2,12 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package dsa.assignment;
+package dsa.asg;
 
-public class DSA_Assignment {
+/**
+ *
+ * @author user
+ */
+public class main {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello How are you? Im Bob!");
+        // TODO code application logic here
     }
     
 }
