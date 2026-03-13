09bd962b8316180294c1c00fd1e37c664131a0a5
Bob Gan <Bob Gan@LAPTOP-CF774SSM>
2026-03-13 15:02:39 +0800

    test3

--- src/dsa_assignment/DSA_Assignment.java
+++ src/dsa_assignment/DSA_Assignment.java
@@ -3,7 +3,6 @@ package dsa_assignment;
 public class DSA_Assignment {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello");
     }
-    
 }
