2ec470af171eb5927369f6ea0ad131c0a6eb3d02
user <user@skullbreaker>
2026-03-17 15:22:39 +0800

    test from kudz

---
+++ src/own/dsa/asg/OwnDsaAsg.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package own.dsa.asg;
+
+/**
+ *
+ * @author user
+ */
+public class OwnDsaAsg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // testing from kudz bla bla bla
+        //////
+    }
+    
+}
